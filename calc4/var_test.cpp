c0977291ae19656ee8f1ccb361e4f59f52a94c5c
Andrew <as10443@nyu.edu>
2018-07-31 16:51:37 +0000

    calculator

---
+++ calc4/var_test.cpp
@@ -0,0 +1,25 @@
+#include "std_lib_facilities.h"
+#include "vars.h"
+
+int main(){
+
+    set_value("foo",12.2);
+    set_value("goo",16.2);
+    set_value("blue", 2.2);
+    set_value("foo",2.9);
+
+
+double d;
+d = get_value("foo");
+cout << "foo = " << d << endl;
+
+d = get_value("goo");
+cout << "goo = " << d << endl;
+
+d = get_value("blue");
+cout << "blue = " << d << endl;
+
+d = get_value("grue");
+cout << "grue = " << d << endl;
+
+}
