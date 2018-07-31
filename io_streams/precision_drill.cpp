943edb57b0c985e12f31f7a9808308e6712e13d1
Andrew <as10443@nyu.edu>
2018-07-31 16:55:06 +0000

    io_streams

---
+++ io_streams/precision_drill.cpp
@@ -0,0 +1,15 @@
+#include "std_lib_facilities.h"
+
+cout << 1234.56789 << '\t' << fixed << 1234.56789 << '\t' << scientific << 1234.56789 << '\n';
+cout << defaultfloat << setprecision(5) << 1234.56789 << '\t' << fixed << 1234.56789 << '\t' << scientific << 1234.56789 << '\n';
+cout << defaultfloat << setprecision(8) << 1234.56789 << '\t' << fixed << 1234.56789 << '\t' << scientific << 1234.56789 << '\n';
+
+
+
+
+
+
+
+
+
+
