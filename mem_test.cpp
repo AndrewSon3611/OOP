e1f6cf47d1f4de868bb4c29e0335f606651f9249
Andrew <as10443@nyu.edu>
2018-08-07 15:02:07 +0000

    memory testing assignment

---
+++ mem_test.cpp
@@ -0,0 +1,20 @@
+#include "std_lib_facilities.h"
+
+int main(){
+    int i = 0;
+    while (true){
+        double* dptr = new double[10000];
+        cout << "loop " << i << endl;
+        cout << "dptr[0]" << dptr[0] << endl;
+        i++;
+    }
+}
+
+
+
+
+
+
+
+
+
